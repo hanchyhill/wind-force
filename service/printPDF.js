355a236b8318e60900ad600bbdb7cec35dfdf3c4
minhill <lrrq369@gmail.com>
2020-06-23 15:30:05 +0800

    change:增加等待时间
    change:增加等待时间

--- service/printPDF.js
+++ service/printPDF.js
@@ -112,7 +112,8 @@ async function main (timeConfig=getTime()) {
       width: 900,//1024,
       height: 768,
     });
-    await timeout(6000);
+    await timeout(10000);
+    console.log('喝杯茶，等待数据中心接口回传')
     console.log('正在生成PDF '+fileName);
     await page.pdf(pdfConfig);
     // 外链
