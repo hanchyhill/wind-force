cbb57f7d30cfbaa7bb499eab5d83c7bd5cf9d604
minhill <lrrq369@gmail.com>
2021-03-04 16:28:02 +0800

    update: 日期修改

---
+++ service/ZHWS_nodePrintPDF.bat
@@ -0,0 +1,12 @@
+echo 珠海万山珠外沙场单点24小时预报
+d:
+cd D:\app\wind-force-YJFD\service
+node ZHWS_printPDF.js
+echo off
+echo ******************************
+echo 珠海万山珠外沙场单点24小时预报已生成，请注意检查！
+echo ******************************
+echo 检查完毕请关闭本窗口
+echo ******************************
+echo on
+pause
