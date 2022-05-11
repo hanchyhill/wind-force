400b16a8f5d3013c7874e9aba11f94b1a82cfc4b
minhill <lrrq369@gmail.com>
2022-05-11 12:45:50 +0800

    add: 粤电青州岛

---
+++ service/YD_QZD_nodePrintPDF.bat
@@ -0,0 +1,12 @@
+echo 徐闻东海采砂场单点24小时预报
+d:
+cd D:\app\wind-force-YJFD\service
+node YD_QZD_printPDF.js
+echo off
+echo ******************************
+echo 徐闻东海采砂场单点24小时预报已生成，请注意检查！
+echo ******************************
+echo 检查完毕请关闭本窗口
+echo ******************************
+echo on
+pause
